28cdd24007c9f7a99e9a792c1fc58aa3637e0a18
ankitarrow <156340025+ankitarrow@users.noreply.github.com>
2024-06-19 14:42:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = require('./routes')
 const bodyParser = require('body-parser')
 const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : true,
     credentials : true
 }))
 app.use(cookieParser())
